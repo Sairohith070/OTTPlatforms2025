f0c0b0ac637bab7d9ace35b09b6a1f000df0dac8
Sairohith070 <sairohithkongari@lewisu.edu>
2025-02-18 19:43:33 -0600

    Add files via upload

---
+++ ott_platform_1.sql
@@ -0,0 +1,6 @@
+insert into ott_platform (ID, OTT_Platform, Total_number_of_contents,  Number_of_users ) values ('1', 'Netflix','27','120');											
+insert into ott_platform (ID, OTT_Platform, Total_number_of_contents,  Number_of_users ) values ('2', 'Amazon Prime','17','112');											
+insert into ott_platform (ID, OTT_Platform, Total_number_of_contents,  Number_of_users ) values ('3', 'HBO max','20','111');											
+insert into ott_platform (ID, OTT_Platform, Total_number_of_contents,  Number_of_users ) values ('4', 'Hulu','17','98');											
+insert into ott_platform (ID, OTT_Platform, Total_number_of_contents,  Number_of_users ) values ('5', 'Disney Plus','17','88');											
+insert into ott_platform (ID, OTT_Platform, Total_number_of_contents,  Number_of_users ) values ('6', 'Ahaa','14','32');											
